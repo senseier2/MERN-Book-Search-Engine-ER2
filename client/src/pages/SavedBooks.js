9d2b43d92c7332a512a02e7cf1a0d891f72fe730
Senfire <eric.mcghee@northwestern.edu>
2022-11-21 17:06:06 -0600

    Updating the savedbooks to uncomment Auth

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,10 +1,10 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
-// import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 // import { deleteBook } from '../utils/API';
 import { removeBookId } from '../utils/localStorage';
 
